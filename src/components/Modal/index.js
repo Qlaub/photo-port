ddedff7f6cc08cbc28a9695d3da907c4ac859b32
Alex Glaubitz <e.pirazzi@gmail.com>
2022-07-03 12:15:02 -0400

    modal component tests

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-function Modal({currentPhoto}) {
+function Modal({ currentPhoto, onClose }) {
   const {name, category, description, index} = currentPhoto;
 
   return (
-    <div className="modalBackDrop">
-      <h3 className='modalTitle'>{name}</h3>
-      <img src={require(`../../assets/large/${category}/${index}.jpg`).default} alt={category} />
-      <p>{description}</p>
-      <button type="button">
-        Close this modal
-      </button>
+    <div className="modalBackdrop">
+      <div className="modalContainer">
+        <h3 className="modalTitle">{name} </h3>
+        <img
+          src={require(`../../assets/large/${category}/${index}.jpg`).default}
+          alt="current category"
+        />
+        <p>{description}</p>
+        <button type="button" onClick={onClose}>
+          Close this modal
+        </button>
+      </div>
     </div>
   );
 }
